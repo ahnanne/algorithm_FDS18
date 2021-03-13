59f4ed8a053a044465305e20068a1ecdeea671ac
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-03-14 00:55:21 +0900

    doubly linked list solution code

--- 210310_Homework/doublyLinkedList.js
+++ 210310_Homework/doublyLinkedList.js
@@ -43,13 +43,16 @@ class DoublyLinkedList {
   }
 
   setHead(index) {
-    if (index <= 0) return false;
-
     let curr = this.head;
     if (!curr) return false;
+    if (index <= 0) return false;
+    // index === 0은 의미가 없으므로 false를 반환하도록 함.
 
     for (let i = 0; i < index; i++) {
       if (!curr.next) return false;
+      // 해당 index로 접근하고자 하는 노드는
+      // 최종적으로 curr.next가 가리키는 노드일 것이므로,
+      // curr.next가 null일 경우 false를 반환하도록 함.
       curr = curr.next;
     }
 
@@ -71,11 +74,47 @@ class DoublyLinkedList {
     return curr;
   }
 
-  /*
   insert(index, value) {
+    const node = new Node(value, null, null);
 
+    let curr = this.head;
+    if (!curr) return false;
+
+    // for문 실행 전 예외 처리
+    // index === 0일 경우
+    if (index === 0) {
+      curr.next.prev = node;
+      node.next = curr.next;
+
+      curr.next = node;
+      node.prev = curr;
+
+      return true;
+    }
+
+    for (let i = 0; i < index; i++) {
+      if (!curr.next) return false;
+      curr = curr.next;
+    }
+
+    if (curr === this.tail) {
+      // for문 실행 후 예외 처리
+      // curr === this.tail일 경우 -> append와 동일
+      curr.next = node;
+      node.prev = curr;
+      this.tail = node;
+    } else {
+      curr.next.prev = node;
+      node.next = curr.next;
+
+      curr.next = node;
+      node.prev = curr;
+    }
+
+    return true;
   }
 
+  /*
   remove(index) {
 
   }
@@ -115,3 +154,9 @@ printTest();
 console.log(testList.access(0));
 console.log(testList.access(1));
 console.log(testList.access(2));
+
+testList.insert(0, 'testData5');
+printTest();
+
+testList.insert(2, 'testData6');
+printTest();
